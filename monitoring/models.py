03ca23f27025c429c1d5e2f599fdc0625d831051
Zuyeonn <joannapark0417@gmail.com>
2026-01-23 15:58:49 +0900

    feat: 보호대상자 db 기본 정보 추가

--- monitoring/models.py
+++ monitoring/models.py
@@ -4,7 +4,11 @@ from django.db import models
 class Protectee(models.Model):
     name = models.CharField(max_length=100)
     # 필요하면 전화번호, 주소 등 추가 가능
-    # phone = models.CharField(max_length=20, null=True, blank=True)
+    device_id=models.CharField(max_length=100, unique=True)
+    gender = models.CharField(max_length=20, null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return f"{self.id} - {self.name}"
@@ -15,7 +19,7 @@ class Event(models.Model):
     class EventType(models.TextChoices):
         PPG_ALERT = "PPG_ALERT", "PPG 알람"
         IMU_ALERT = "IMU_ALERT", "IMU 알람"
-        GEO_ALERT = "GEO_ALERT", "위치 알람"
+        GEO_ALERT = "GEO_ALERT", "GEO 알람"
 
     protectee = models.ForeignKey(Protectee, on_delete=models.CASCADE)
     timestamp = models.DateTimeField()
@@ -24,15 +28,13 @@ class Event(models.Model):
     # IMU
     imu_danger_level = models.IntegerField(null=True, blank=True)
 
+    #PPG
+    ppg_holding = models.IntegerField(null=True, blank=True)
+
     # GEO
     latitude = models.FloatField(null=True, blank=True)
     longitude = models.FloatField(null=True, blank=True)
-    is_usual_route = models.IntegerField(
-        null=True, blank=True, help_text="1=평소경로, 0=이탈"
-    )
 
-    # 확장용
-    meta_json = models.JSONField(null=True, blank=True)
 
     class Meta:
         indexes = [
