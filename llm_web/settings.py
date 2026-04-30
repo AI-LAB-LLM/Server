2919e91fff821a2cd1c7e2e0cb510a70b90439a7
Zuyeonn <joannapark0417@gmail.com>
2026-04-30 16:50:11 +0900

    feat: media 폴더 경로 설정

--- llm_web/settings.py
+++ llm_web/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = env("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost","210.125.91.90"])
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Application definition
 
